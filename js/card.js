6a38f746a898810ee6cb27ab39572ef4fc13904d
Sergey Pashin <S.E.Pashin@yandex.ru>
2020-09-25 19:48:52 +0300

    правка

--- js/card.js
+++ js/card.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function () {
-  var addOpeningProperty = function (data) { /* Добавлено в pin renderPinCards */
+  var addOpeningProperty = function (data) { /* Добавление слушателя на каждое объявление Добавлено в pin renderPinCards */
 
     // Необходимо наполнить действиями с карточкой объявлений.
     var onCardVisible = function (evt) { /* evt мгновенный снимок того с чем только что произошло событие именно в этот момент! */
@@ -16,18 +16,19 @@
       if(targetCard.offer.title) {templatePopupClone.querySelector('.popup__title').textContent = targetCard.offer.title}; /* Протестировано*/
       if(targetCard.offer.address) {templatePopupClone.querySelector('.popup__text--address').textContent = targetCard.offer.address}; /* Адрес Протестировано*/
       if(targetCard.offer.price) {templatePopupClone.querySelector('.popup__text--price').textContent = targetCard.offer.price + ' ₽/ночь'}; /* Цена Протестировано*/
+      if(targetCard.offer.rooms && targetCard.offer.guests) {templatePopupClone.querySelector('.popup__text--capacity').textContent = targetCard.offer.rooms + ' комнаты для ' + targetCard.offer.guests + ' гостей.'}; /* количество гостей и комнат Протестировано*//* TODOF в будущем, можно улучшить условием если больше какого то числа то окончание камнат/ы изменяется */
+      if(targetCard.offer.checkin && targetCard.offer.checkout) {templatePopupClone.querySelector('.popup__text--time').textContent = 'Заезд после ' +  targetCard.offer.checkin + ', выезд до ' + targetCard.offer.checkout}; /* Время заезда и выезда Протестировано*/
+      if(targetCard.offer.features) {templatePopupClone.querySelector('.popup__features').textContent = targetCard.offer.features}; /* все доступные удобства в объявлении. Протестировано*/
+      if(targetCard.offer.description) {templatePopupClone.querySelector('.popup__description').textContent = targetCard.offer.description}; /* описание объекта недвижимости Протестировано*/
+      if(targetCard.author.avatar) {templatePopupClone.querySelector('.popup__avatar').src = targetCard.author.avatar};  /* src у аватарки Протестировано*/
 
       var apparmentType = {
           flat: 'Квартира',
           bungalo: 'Бунгало',
           house: 'Дом',
           palace: 'Дворец'
-      }
+      };
       if(targetCard.offer.type) {templatePopupClone.querySelector('.popup__type').textContent = apparmentType[targetCard.offer.type]}; /* Тип апартаментов Протестировано*/
-      if(targetCard.offer.rooms && targetCard.offer.guests) {templatePopupClone.querySelector('.popup__text--capacity').textContent = targetCard.offer.rooms + ' комнаты для ' + targetCard.offer.guests + ' гостей.'}; /* количество гостей и комнат Протестировано*//* TODOF в будущем, можно улучшить условием если больше какого то числа то окончание камнат/ы изменяется */
-      if(targetCard.offer.checkin && targetCard.offer.checkout) {templatePopupClone.querySelector('.popup__text--time').textContent = 'Заезд после ' +  targetCard.offer.checkin + ', выезд до ' + targetCard.offer.checkout}; /* Время заезда и выезда Протестировано*/
-      if(targetCard.offer.features) {templatePopupClone.querySelector('.popup__features').textContent = targetCard.offer.features}; /* все доступные удобства в объявлении. Протестировано*/
-      if(targetCard.offer.description) {templatePopupClone.querySelector('.popup__description').textContent = targetCard.offer.description}; /* описание объекта недвижимости Протестировано*/
 
       var photosPopup = templatePopupClone.querySelector('.popup__photos');
       var photoPopup = templatePopup.querySelector('.popup__photo');
@@ -41,12 +42,16 @@
       };
       if(targetCard.offer.photos) {getPhotosPopup(targetCard.offer.photos)};  /* Протестировано /* В блок .popup__photos выведите все фотографии из списка offer.photos. Каждая из строк массива photos должна записываться как src соответствующего изображения. */
 
-      if(targetCard.author.avatar) {templatePopupClone.querySelector('.popup__avatar').src = targetCard.author.avatar};  /* src у аватарки Протестировано*/
 
       var mapPins = document.querySelector('.map__pins');
       mapPins.after(templatePopupClone); /* 5. Хочу  Отобразить полученный шаблон с внесенными в него данными на странице в виде попапа*/
 
       /* TODO 6. Хочу Удалить данный шаблон если происходит какой либо клик вне поля данного объявления */
+
+      // var onClickClose = function () {
+
+      // };
+
     };
 
     var mapPinNoMainNew = document.querySelectorAll('.map__pin:not(.map__pin--main)');
