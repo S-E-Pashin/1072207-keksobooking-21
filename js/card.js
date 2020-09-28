d05b0831a486afb6383cc8a773f10f0fe85d6066
Sergey Pashin <S.E.Pashin@yandex.ru>
2020-09-29 01:42:51 +0300

    Внесены изменения в блок карточки попапа, для корректного отображения фотографий, корректного отображения значков удобств номеров, корректировка взаимодействия с кнопкой в которой находятся изображение

--- js/card.js
+++ js/card.js
@@ -10,15 +10,49 @@
       var templatePopup = document.querySelector('#card').content.querySelector('.popup'); /* 1. хочу получить шаблон который смогу заполнить данными полученными от метки на которую было выполнено нажатие. */
       var templatePopupClone = templatePopup.cloneNode(true); /* 2. Хочу сделать клон указанного шаблона */
       var targetCard = data.find(function (card) { /* Поиск элемента поле offer.id которого совпадает с id-ом  */ /* Функция вызывается на массиве, переберает его элементы на предмет соответствия указанного нами значения(В частности evt.target.id-мгновенное значение из обекта с которым было выполнено взаимодействие), при совпадении с данным элементом вернет объект в котором он находится в переменную targetCard */
-        return card.author.id == evt.target.id;
+        if (evt.target.type == 'button') {
+          var searchImgId = evt.target.querySelector('img');
+          return card.author.id == searchImgId.id;
+        } else {
+          return card.author.id == evt.target.id;
+        }
       });
-
       if(targetCard.offer.title) {templatePopupClone.querySelector('.popup__title').textContent = targetCard.offer.title}; /* Протестировано*/
       if(targetCard.offer.address) {templatePopupClone.querySelector('.popup__text--address').textContent = targetCard.offer.address}; /* Адрес Протестировано*/
       if(targetCard.offer.price) {templatePopupClone.querySelector('.popup__text--price').textContent = targetCard.offer.price + ' ₽/ночь'}; /* Цена Протестировано*/
       if(targetCard.offer.rooms && targetCard.offer.guests) {templatePopupClone.querySelector('.popup__text--capacity').textContent = targetCard.offer.rooms + ' комнаты для ' + targetCard.offer.guests + ' гостей.'}; /* количество гостей и комнат Протестировано*//* TODOF в будущем, можно улучшить условием если больше какого то числа то окончание камнат/ы изменяется */
       if(targetCard.offer.checkin && targetCard.offer.checkout) {templatePopupClone.querySelector('.popup__text--time').textContent = 'Заезд после ' +  targetCard.offer.checkin + ', выезд до ' + targetCard.offer.checkout}; /* Время заезда и выезда Протестировано*/
-      if(targetCard.offer.features) {templatePopupClone.querySelector('.popup__features').textContent = targetCard.offer.features}; /* все доступные удобства в объявлении. Протестировано*/
+
+      var featuresClass = {
+        wifi: 'popup__feature--wifi',
+        dishwasher: 'popup__feature--dishwasher',
+        parking: 'popup__feature--parking',
+        washer: 'popup__feature--washer',
+        elevator: 'popup__feature--elevator',
+        conditioner: 'popup__feature--conditioner'
+      }
+
+      if (targetCard.offer.features.length !== 0) {
+        console.log(targetCard.offer.features);
+        targetCard.offer.features.forEach(function (el) {
+          var cloneFeature = templatePopup.querySelector('.popup__feature').cloneNode(true);
+          cloneFeature.classList.add(featuresClass[el]);
+          templatePopupClone.querySelector('.popup__features').appendChild(cloneFeature);
+        });
+        templatePopupClone.querySelectorAll('.popup__feature')[0].remove();
+      } else {
+        templatePopupClone.querySelector('.popup__feature').remove();
+      }
+
+      // <ul class="popup__features">
+      //   <li class="popup__feature popup__feature--wifi"></li>
+      //   <li class="popup__feature popup__feature--dishwasher"></li>
+      //   <li class="popup__feature popup__feature--parking"></li>
+      //   <li class="popup__feature popup__feature--washer"></li>
+      //   <li class="popup__feature popup__feature--elevator"></li>
+      //   <li class="popup__feature popup__feature--conditioner"></li>
+      // </ul>
+
       if(targetCard.offer.description) {templatePopupClone.querySelector('.popup__description').textContent = targetCard.offer.description}; /* описание объекта недвижимости Протестировано*/
       if(targetCard.author.avatar) {templatePopupClone.querySelector('.popup__avatar').src = targetCard.author.avatar};  /* src у аватарки Протестировано*/
 
@@ -30,18 +64,25 @@
       };
       if(targetCard.offer.type) {templatePopupClone.querySelector('.popup__type').textContent = apparmentType[targetCard.offer.type]}; /* Тип апартаментов Протестировано*/
 
-      var photosPopup = templatePopupClone.querySelector('.popup__photos');
-      var photoPopup = templatePopup.querySelector('.popup__photo');
-      var photoPopupClone = photoPopup.cloneNode(true);
+      // var photosPopup = templatePopupClone.querySelector('.popup__photos');
+      // var photoPopup = templatePopup.querySelector('.popup__photo');
+      // var photoPopupClone = photoPopup.cloneNode(true);
 
-      var getPhotosPopup = function (photoMassive) {
-        photoMassive.forEach(function (value) {
-          photoPopupClone.src = value;
-          photosPopup.appendChild(photoPopupClone);
-        })
+      // var getPhotosPopup = function (photoMassive) {
+      //   photoMassive.forEach(function (value) {
+      //     photoPopupClone.src = value;
+      //     photosPopup.appendChild(photoPopupClone);
+      //   })
+      // };
+      // if(targetCard.offer.photos) {getPhotosPopup(targetCard.offer.photos)};  /* Протестировано /* В блок .popup__photos выведите все фотографии из списка offer.photos. Каждая из строк массива photos должна записываться как src соответствующего изображения. */
+
+      if (targetCard.offer.photos.length !== 0) {
+        targetCard.offer.photos.forEach(function (el) {
+          templatePopup.querySelector('.popup__photo').src = el;
+          templatePopupClone.querySelector('.popup__photos').appendChild(templatePopup.querySelector('.popup__photo').cloneNode(true));
+        });
+        templatePopupClone.querySelectorAll('.popup__photo')[0].remove();
       };
-      if(targetCard.offer.photos) {getPhotosPopup(targetCard.offer.photos)};  /* Протестировано /* В блок .popup__photos выведите все фотографии из списка offer.photos. Каждая из строк массива photos должна записываться как src соответствующего изображения. */
-
 
       var mapPins = document.querySelector('.map__pins');
       mapPins.after(templatePopupClone); /* 5. Хочу  Отобразить полученный шаблон с внесенными в него данными на странице в виде попапа*/
