f195ea6ac01399239ab8df602a7016ad7b8d2d9d
Sergey Pashin <S.E.Pashin@yandex.ru>
2020-10-23 12:55:50 +0300

    Дебаунс почищен от комментариев.

--- js/debounce.js
+++ js/debounce.js
@@ -1,27 +1,5 @@
 /* eslint-disable no-var */
 'use strict';
-// (function () {
-//   var DEBOUNCE_INTERVAL = 300;
-
-//   window.debounce = function (cb) {
-//     var lastTimeout = null;
-
-//     return function () {
-//       var parameters = arguments;
-//       if (lastTimeout) {
-//         window.clearTimeout(lastTimeout);
-//       }
-//       lastTimeout = window.setTimeout(function () {
-//         cb.apply(null, parameters);
-//       }, DEBOUNCE_INTERVAL);
-//     };
-//   };
-
-//   // window.debounce =  {
-//   //   debounce: debounce
-//   // };
-// })();
-
 (function () {
   var DEBOUNCE_INTERVAL = 300; // ms
 
@@ -38,23 +16,3 @@
     };
   };
 })();
-
-// 'use strict';
-// (function () {
-//   var DEBOUNCE_INTERVAL = 500;
-
-//   function debounce(cb) {
-//     var lastTimeout = null;
-//     return function () {
-//       var args = arguments;
-//       if (lastTimeout) {
-//         window.clearTimeout(lastTimeout);
-//       }
-//       lastTimeout = window.setTimeout(function () {
-//         cb.apply(null, args);
-//       }, DEBOUNCE_INTERVAL);
-//     };
-//   }
-
-//   window.debounce = debounce;
-// })();
