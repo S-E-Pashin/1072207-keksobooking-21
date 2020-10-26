a8135f75bff466f59b8f20c429de3100782bfae2
Sergey Pashin <S.E.Pashin@yandex.ru>
2020-10-26 18:25:33 +0300

    map почищен от лишних комментариев, убрана не работающая функция.

--- js/map.js
+++ js/map.js
@@ -1,27 +1,15 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable no-var */
 'use strict';
 
 (function () {
   var map = document.querySelector('.map');
-  // НЕАКТИВНОЕ СОСТОЯНИЕ:
   map.classList.add('map--faded'); /* Добавляется неактивность для карты и содержимого */
-  // АКТИВНОЕ СОСТОЯНИЕ
   var removeAttributeDisabled = function (liveCollection) { /*  Функция поочередно удаляющая атрибуты неактивности к полям */
     for (var i = 0; i < liveCollection.length; i++) { /* Цикл для удаления  атрибута к полям */
       liveCollection[i].removeAttribute('disabled', 'true'); /* Поочередное удаление атрибута к каждому филдсету полей. */
     }
     return liveCollection;
   };
-
-  /* Почему не работает? */
-  /*   var addAttributeDisabled = function (liveCollection) {
-    liveCollection.forEach(function (value) {
-      value.setAttribute('disabled', 'disabled');
-    });
-    return liveCollection;
-  }; */
-
 
   var setAttributeDisabled = function (liveCollection) { /*  Функция поочередно удаляющая атрибуты неактивности к полям */
     for (var i = 0; i < liveCollection.length; i++) { /* Цикл для удаления  атрибута к полям */
@@ -32,7 +20,6 @@
 
   window.map = {
     removeAttributeDisabled: removeAttributeDisabled,
-    // addAttributeDisabled: addAttributeDisabled,
     setAttributeDisabled: setAttributeDisabled
   };
 })();
