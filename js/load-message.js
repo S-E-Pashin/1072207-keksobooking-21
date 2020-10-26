4761ebc774258bc80f964741ee37ea3e28001d7e
Sergey Pashin <S.E.Pashin@yandex.ru>
2020-10-26 18:23:01 +0300

    load-message почищен он лишних комментариев.

--- js/load-message.js
+++ js/load-message.js
@@ -1,8 +1,6 @@
 /* eslint-disable no-var */
 'use strict';
 (function () {
-  /* Добавьте обработку возможных ошибок при загрузке: создайте DOM-элемент, который будет показывать сообщения об ошибках, произошедших по ходу загрузки данных. Дизайн DOM-элемента предлагается придумать самостоятельно. */
-
   var errorTemplate = document.querySelector('#error').content.querySelector('.error');
   var errorLoadTemplateClone = errorTemplate.cloneNode(true);
 
@@ -15,14 +13,11 @@
   };
 
   var getErrorPopup = function (message) {
-    // console.log('Сработал попап ошибки загрузки');
     errorLoadTemplateClone.querySelector('.error__message').textContent = message;
     document.querySelector('main').appendChild(errorLoadTemplateClone);
     document.addEventListener('keydown', onCloseErrorLoadPopup);
     document.addEventListener('click', onCloseErrorLoadPopup);
   };
-
-  // getErrorPopup();/* Запускатор функции для комфортного взаимодействия с функцией и ее теста. */
 
   window.loadMessage = {
     getErrorPopup: getErrorPopup
