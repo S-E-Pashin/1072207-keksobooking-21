443e7a9bb007661c8c065cd1ca0215ceb201da9d
Sergey Pashin <S.E.Pashin@yandex.ru>
2020-10-26 18:29:17 +0300

    move удалены лишние комментарии, оставлена часть закомментированного кода который должен быть удален перед релизом.

--- js/move.js
+++ js/move.js
@@ -38,8 +38,6 @@
       };
       var onMouseMove = function (moveEvt) {
         moveEvt.preventDefault(); /* Отменили действие по умолчанию для движения. */
-        // mapPinMain.removeEventListener('click', window.pin.onMainPinMouseOrKeyDown); /* Удалил обработчик клика при начале движения. */
-
 
         var shift = {
           x: startCoords.x - moveEvt.clientX,
@@ -61,7 +59,6 @@
 
 
         if (mapPinMain.offsetTop > MAX_Y_TOP) {
-        // mapPinMain.style.top = (mapPinMain.offsetTop - shift.y) + 'px';
           movingAxisY();
         } else {
           mapPinMain.style.top = MAX_Y_TOP + 'px';
@@ -89,20 +86,6 @@
       };
       var onMouseUp = function (upEvt) { /* Действия при поднятии клавиши. будет удален слушатель перемещения курсора мыши и удалит слушатель поднятия клавиши сам себя. */
         upEvt.preventDefault(); /* Перестал слушать движения мыши */
-        /* !!! Вообще не нужное теперь условие и функция */
-        // if (!dragged) { /* Получил подтверждение что движения были. */
-        // // Обсудить отдельно потом.
-        //   console.log('Движения были');
-        //   var onClickPreventDefault = function (clickEvt) {
-        //     clickEvt.preventDefault();
-        //     mapPinMain.addEventListener('click', window.pin.onMainPinMouseOrKeyDown); /* Добавлен слушатель/обработчик на событие click + клик левой клавишей мыши*/
-        //     // mapPinMain.removeEventListener('click', onClickPreventDefault);
-        //     mapPinMain.removeEventListener('click', onClickPreventDefault);
-        //   // document.removeEventListener('click', onClickPreventDefault);
-        //   };
-        //   mapPinMain.addEventListener('click', onClickPreventDefault);
-        // // mapPinMain.addEventListener('click', window.pin.onMainPinMouseOrKeyDown); /* Добавлен слушатель/обработчик на событие click + клик левой клавишей мыши*/
-        // }
         document.removeEventListener('mousemove', onMouseMove); /* Удаляется обработчик движения для прекращения движения элемента */
         document.removeEventListener('mouseup', onMouseUp);/* Удаляет сам себя - удаляет слушатель/обработчик поднятия клавиши на документе. */
       };
@@ -110,19 +93,20 @@
       document.addEventListener('mouseup', onMouseUp);
     }
   };
-  // console.log(dragged);
+
   var activeMainPinRestart = function () {
     dragged = true;
   };
+
   var activeMainPin = function () {
     mapPinMain.addEventListener('mousedown', onMapPinMainPress);
     mapPinMain.addEventListener('keydown', onMapPinMainPress);
   };
+
   activeMainPin();
 
   window.move = {
     activeMainPin: activeMainPin,
     activeMainPinRestart: activeMainPinRestart
-    // onMapPinMainPress: onMapPinMainPress
   };
 })();
