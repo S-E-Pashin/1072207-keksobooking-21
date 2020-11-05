e7bba5f5068e913d720368dd7831ef2fa6b47db4
Sergey Pashin <S.E.Pashin@yandex.ru>
2020-11-05 20:11:39 +0300

    Изменения в мастер.

--- js/pin.js
+++ js/pin.js
@@ -11,20 +11,17 @@
   var renderPinCloneTemplateElements = function (item) { /* Относится к Рендеру Это функция которая сначала создает клон а потом просто присваивает этому клону необходимые значения. */
     var pinCloneTemplate = templatePin.cloneNode(true); /* Создаем переменную в которую записываем/копируем/клонируем элемент/переменную/Шаблон template со всем ее содержимым(Т.е. всю ее разметку вместе с детьми/если бы были.(true), если дети узла должны быть клонированы или false для того, чтобы был клонирован только указанный узел.) */
     var pinCloneTemplateImage = pinCloneTemplate.querySelector('img');
-
     pinCloneTemplateImage.src = item.author.avatar;
     pinCloneTemplateImage.alt = item.offer.title;
     pinCloneTemplateImage.id = item.author.id; /* Счетчик для id */
     pinCloneTemplate.style.left = (item.location.x - WIDTH_AVATAR / 2) + 'px';/* Изменение по оси x. Ширина объекта исходя из разметки составляет 50px. Т.к. точка 0 для отрисованного объекта в разметке находится в левом верхнем углу фигуры мы понимаем что нам необходимо сместить фигуру для получения нахождения острия отображаемого изображения на половину ее ширины, тем самым разделив ее ширину пополам. Для этого будет заведена константа WIDTH_AVATAR и разделена пополам непосредственно в выполняемом выражении а после вычтена из создаваемой координаты для определения положения элемента на оси Х.  */
     pinCloneTemplate.style.top = (item.location.y - HEIGHT_AVATAR) + 'px'; /* Высота = 70px Смещение по вертикали на высоту элемента путем вычитания из положения по оси У целого значения высоты элемента.*/
-
     return pinCloneTemplate;
   };
 
   var templatePin = document.querySelector('#pin').content.querySelector('.map__pin');/* ДОБАВЛЕНИЕ ЧЕРЕЗ template Создана переменная template(одноименна с названием элемента template) которая ищет элемент/шаблон template по id, после обращается к свойству данного элемента content(которое является единственным свойством данного элемента и предназначено для взаимодействия с его содержимым.) */
   var fragment = document.createDocumentFragment();
   var mapPins = document.querySelector('.map__pins'); /* Переменная для нахождения блока с классом map__pins. (в последующем будет использоваться для добавления элементов в разметку посредством documentFragment)Это блок для отрисовки. */
-
 
   var removeOlds = function () { /* Функция удаляющая пины из разметки */
     var mapPinsNoMain = document.querySelectorAll('.map__pin:not(.map__pin--main)');
