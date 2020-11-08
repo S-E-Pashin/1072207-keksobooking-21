ef85c9660185ad30f1a631a8daf50190366696db
Sergey Pashin <S.E.Pashin@yandex.ru>
2020-11-08 14:19:20 +0300

    Превью выполнено, закомментированные участки убраны.

--- js/preview-image.js
+++ js/preview-image.js
@@ -1,128 +1,6 @@
 /* eslint-disable no-var */
 'use strict';
 (function () {
-  /* №№№№№№№№№№№№№№№№№№№№№№№№№№№ Работающий вариант для поля аватара ВАРИАНТ 1. */
-  // var IMAGE_PHOTO_HEIGHT = '70';
-  // var IMAGE_PHOTO_WIDTH = '70';
-  // var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
-  // // var fileAvatar = document.querySelector('.ad-form__field input[type=file]');
-  // var fileAvatar = document.querySelector('.ad-form-header__input');
-  // var preview = document.querySelector('.ad-form-header__preview img');
-
-  // var getFileChoice = function () { /* Функция которая подставит выбранный пользователем файл изображения из поля выбора в место отображения картинки с заданными условиями. */
-  //   var file = fileAvatar.files[0]; /* Приравниваю значение file к значению файла который выбран в поле(Данное свойство всегда выглядит как массив.) */
-  //   var fileName = file.name.toLowerCase(); /* Трансформирую имя файла для однотипного его восприятия. Приведен к строковым/маленьким.  */
-  //   var matches = FILE_TYPES.some(function (it) { /* Проверка оканчивает ли имя файла на заданный тип. it он же ending. some это метод который будет вызван на каждый элемент массива FILE_TYPES и если строка оканчивается на переданую подстроку то тогда, если хоть один из вариантов константы(окончаний) подойдет/будет равен окончанию имени файла то он вернет true */
-  //     return fileName.endsWith(it); /* Проверяет на соответствие заданным константам. */
-  //   });
-
-  //   if (matches) { /* Если условие верно и файл оканчивается на требуемый/заданный для нас, тогда. */
-  //     var reader = new FileReader(); /* Получаю экземпляр ридера(некую копию) с помощью ключевого слова NEW. Ридер=Читатель, своя копия читателя посредством которой будем читать файлы пользователя. */
-
-  //     var onReaderLoad = function () { /* Обработчик загрузки ридера.  */
-  //       preview.src = reader.result; /* Передаю значения в картинку/в атрибут  результат который будет получен при наступлении события load которое в свою очередь будет запущено reader.readAsDataURL(file); */
-  //     };
-
-  //     reader.addEventListener('load', onReaderLoad); /* Событие load вызовется в тот момент как ридер прочтет файл который загрузил/выбрал пользователь. */
-
-  //     reader.readAsDataURL(file); /* readAsDataURL - Метод - прочти как DATA URL. И дальше он открывает файл выбранный пользователем. */
-  //   }
-  // };
-
-  // var onFileChoice = function () {
-  //   getFileChoice();
-  // };
-
-  // fileAvatar.addEventListener('change', onFileChoice);
-
-  /* №№№№№№№№№№№№№№№№№№№ КОНЕЦ ВАРИАНТА 1*/
-
-
-  // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  ВАРИАНТ 3  НАЧАЛО ПОЛЕ ФОТОГРАФИЙ ЖИЛЬЯ C изображением в img  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
-  // var fileAvatar = document.querySelector('.ad-form__field input[type=file]');
-  // var fileGallery = document.querySelector('.ad-form__input');
-  // var previewUpload = document.querySelector('.ad-form__photo');
-
-  // var getFileChoiceUpload = function () { /* Функция которая подставит выбранный пользователем файл изображения из поля выбора в место отображения картинки с заданными условиями. */
-  //   var file = fileGallery.files[0]; /* Приравниваю значение file к значению файла который выбран в поле(Данное свойство всегда выглядит как массив.) */
-  //   var fileName = file.name.toLowerCase(); /* Трансформирую имя файла для однотипного его восприятия. Приведен к строковым/маленьким.  */
-  //   var matches = FILE_TYPES.some(function (it) { /* Проверка оканчивает ли имя файла на заданный тип. it он же ending. some это метод который будет вызван на каждый элемент массива FILE_TYPES и если строка оканчивается на переданую подстроку то тогда, если хоть один из вариантов константы(окончаний) подойдет/будет равен окончанию имени файла то он вернет true */
-  //     return fileName.endsWith(it); /* Проверяет на соответствие заданным константам. */
-  //   });
-
-  //   if (matches) { /* Если условие верно и файл оканчивается на требуемый/заданный для нас, тогда. */
-  //     var reader = new FileReader(); /* Получаю экземпляр ридера(некую копию) с помощью ключевого слова NEW. Ридер=Читатель, своя копия читателя посредством которой будем читать файлы пользователя. */
-
-  //     var getImg = function () {
-  //       var imagePhoto = document.createElement('img');
-  //       imagePhoto.src = reader.result;
-  //       imagePhoto.width = IMAGE_PHOTO_WIDTH;
-  //       imagePhoto.height = IMAGE_PHOTO_HEIGHT;
-  //       previewUpload.appendChild(imagePhoto);
-  //     };
-
-  //     var removeImg = function () {
-  //       if (previewUpload.querySelector('img')) {
-  //         previewUpload.querySelector('img').remove();
-  //       }
-  //     };
-
-  //     var onReaderUpload = function () { /* Обработчик загрузки ридера.  */
-  //       removeImg();
-  //       getImg();
-  //     };
-
-  //     reader.addEventListener('load', onReaderUpload); /* Событие load вызовется в тот момент как ридер прочтет файл который загрузил/выбрал пользователь. */
-
-  //     reader.readAsDataURL(file); /* readAsDataURL - Метод - прочти как DATA URL. И дальше он открывает файл выбранный пользователем. */
-  //   }
-  // };
-
-  // var onfileGallery = function () {
-  //   getFileChoiceUpload();
-  // };
-
-  // fileGallery.addEventListener('change', onfileGallery);
-
-
-  // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  ВАРИАНТ 3 КОНЕЦ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Вариант 4 СОВМЕЩЕНИЕ НАЧАЛО %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
   var IMAGE_PHOTO_HEIGHT = '70';
   var IMAGE_PHOTO_WIDTH = '70';
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
@@ -188,7 +66,6 @@
     getFileChoiceNew(fileGallery);
   };
 
-
   var getFileChoice = function () {
     fileAvatar.addEventListener('change', onFileChoiceAvatar);
     fileGallery.addEventListener('change', onFileChoiceGallery);
@@ -201,7 +78,6 @@
     removeImg();
   };
 
-  // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Вариант 4 СОВМЕЩЕНИЕ КОНЕЦ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
   window.previewImage = {
     getFileChoice: getFileChoice,
