42d6dab55d08e458c74b8b8f65669acf07c554df
Sergey Pashin <S.E.Pashin@yandex.ru>
2020-11-11 12:53:17 +0300

    Внесены изменения для корректировки отображения положения главного пина в поле адреса.

--- js/address.js
+++ js/address.js
@@ -2,7 +2,8 @@
 /* eslint-disable no-var */
 'use strict';
 
-var MAP_PIN_MAIN_AFTER_TIP = 22; /* Высота ножки/острия для метки(Пина) */
+var MAP_PIN_MAIN_AFTER_TIP = 16; /* Высота ножки/острия для метки(Пина) c учетом отступов. */
+var MAP_PIN_MAIN_CORRECTION_EQUATING_TO_ZERO = 1;
 var mapPinMain = document.querySelector('.map__pin--main'); /* Главный пин на карте */
 var mapPinMainAddress = document.querySelector('#address'); /* Адрес(Поле) куда передаются данные о нахождении главного пина(Координаты) */
 
@@ -22,8 +23,8 @@ var returnFirstCoordsMapPinMain = function () { /* Функция возвращ
   getStartCoords();
 };
 
-var getMoveCoords = function () { /* // Координаты центра для иглы метки: map__pin--main */
-  mapPinMainAddress.value = Math.round(mapPinMain.offsetLeft - mapPinMain.offsetWidth / 2) + ', ' + Math.round(mapPinMain.offsetTop - (mapPinMain.offsetHeight / 2 + MAP_PIN_MAIN_AFTER_TIP));/* Вычитание из расстояния сверху до метки половины высоты(оставшейся половины высоты метки) и высоты дополнительного визуального элемента ножки/острия выполненного псевдоэлементом.  */
+var getMoveCoords = function () {
+  mapPinMainAddress.value = Math.round(mapPinMain.offsetLeft + mapPinMain.offsetWidth / 2 - MAP_PIN_MAIN_CORRECTION_EQUATING_TO_ZERO) + ', ' + Math.round(mapPinMain.offsetTop + (mapPinMain.offsetHeight + MAP_PIN_MAIN_AFTER_TIP));
 };
 
 window.address = {
